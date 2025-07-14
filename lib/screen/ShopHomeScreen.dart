9ce1dd7d326bef339246b7b90049a334d8b9a946
diaa <diaahelmy9900@gmail.com>
2025-07-14 23:17:30 +0300

    sginOut and Remove Cache

--- lib/screen/ShopHomeScreen.dart
+++ lib/screen/ShopHomeScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shopapp/componant/shopappcomponat.dart';
+import 'package:shopapp/log_addacount/loginScreen.dart';
+import 'package:shopapp/network/local/Cache.dart';
 
 class ShopHomescreen extends StatelessWidget {
   const ShopHomescreen({super.key});
@@ -7,14 +10,20 @@ class ShopHomescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         centerTitle: true,
-        title: Text('My Shop',style: TextStyle(
-
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-        ),),
+        title: Text(
+          'My Shop',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+      ),
+      body: defaultButton(
+        label: 'SignOut',
+        onPressed: () {
+          Cache.removeData(key: 'token').then((onValue) {
+            navigateAndFinsh(context, LoginScreen());
+          });
+        },
       ),
     );
   }
